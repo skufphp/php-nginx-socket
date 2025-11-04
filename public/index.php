31e481c1da600d2b53553aff539d73127f6c2fc2
Vladimir Matkovskii <vlavlamat@icloud.com>
2025-11-03 20:17:28 -0400

    Fix broken `phpinfo.php` link in `index.php`.

--- public/index.php
+++ public/index.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 echo "<h1>PHP работает ✅</h1>";
 echo "<p>Это index.php, обработанный PHP-FPM через Unix-сокет.</p>";
-echo '<p>См. также <a href="/phpinfo.php">phpinfo()</a>.</p>';
+echo '<p>См. также <a href="phpinfo.php">phpinfo()</a>.</p>';
